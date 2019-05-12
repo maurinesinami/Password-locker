0e9e7c2668e9533bc39e962482ac82f622c24403
maurinenami <maurinenami>
2019-05-12 17:52:53 +0300

    indent the sixth test

--- passwords_test.py
+++ passwords_test.py
@@ -74,7 +74,7 @@ class TestUser(unittest.TestCase):
         user_exists = User.user_exists("Facebook")
         self.assertTrue(user_exists)
 
-      def test_display_all_users(self):
+    def test_display_all_users(self):
         '''
         method that returns a list of all users saved
         '''
